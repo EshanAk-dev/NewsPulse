dff5beaa45070b093b9eb7c8d9988b2db432dedc
EshanAkalanka <163629582+EshanAk44@users.noreply.github.com>
2025-01-15 12:27:45 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/newsapplication/MainActivity.kt
+++ app/src/main/java/com/example/newsapplication/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.newsapplication
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private var backPressedTime: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,5 +28,18 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, RegisterActivity::class.java))
             finish()
         }
+    }
+
+    override fun onBackPressed() {
+        // Check if the back button is pressed within 2 seconds
+        if (backPressedTime + 2000 > System.currentTimeMillis()) {
+            super.onBackPressed() // Exit the activity
+            return
+        } else {
+            Toast.makeText(this, "Press back again to exit", Toast.LENGTH_SHORT).show()
+        }
+
+        // Update backPressedTime to current time
+        backPressedTime = System.currentTimeMillis()
     }
 }
